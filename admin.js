87b4f7bb661bcb81bba64672253283850c9f7777
Marco Portacio <marcoportacio4@gmail.com>
2023-11-13 00:02:32 -0500

    se ha hecho dinamica la pagina

---
+++ admin.js
@@ -0,0 +1,46 @@
+let $tabla_reservas = document.querySelector("#tabla_reservas")
+let reservas = JSON.parse(localStorage.getItem("reservas"))
+
+reservas.forEach(reserva => {
+  $tabla_reservas.appendChild(crearReservas(reserva))
+})
+
+
+function crearReservas(reserva) {
+  let $reserva = document.createElement("tr")
+
+  let $cedula = document.createElement("td")
+  $cedula.textContent = reserva.cedula
+  $reserva.appendChild($cedula)
+
+  let $id_pel = document.createElement("td")
+  $id_pel.textContent = reserva.id_pel
+  $reserva.appendChild($id_pel)
+
+  let $nombre = document.createElement("td")
+  $nombre.textContent = reserva.nombre
+  $reserva.appendChild($nombre)
+
+  let $horario = document.createElement("td")
+  $horario.textContent = reserva.horario
+  $reserva.appendChild($horario)
+
+  let $acciones = document.createElement("td")
+  let $btn_eliminar = document.createElement("button")
+  $btn_eliminar.textContent = "Eliminar"
+  $btn_eliminar.onclick = function () {
+    eliminarReserva(reserva)
+  }
+  $acciones.appendChild($btn_eliminar)
+  $reserva.appendChild($acciones)
+
+  return $reserva
+}
+
+function eliminarReserva(reserva) {
+  let reservas = JSON.parse(localStorage.getItem("reservas"))
+  let index = reservas.findIndex(r => r.cedula == reserva.cedula && r.id_pel == reserva.id_pel && r.horario == reserva.horario)
+  reservas.splice(index, 1)
+  localStorage.setItem("reservas", JSON.stringify(reservas))
+  location.reload()
+}
---
+++ archivo.js
@@ -0,0 +1,163 @@
+var peliculas = [
+    {
+        id: 1,
+        titulo: "La probabilidad estadistica del amor a primera vista",
+        imagenPrincipal: "https://infoliteraria.com/wp-content/uploads/2022/05/la-probabilidad-estaditica.jpg.webp",
+        descripcion: "Hadley y Oliver comienzan a enamorarse lentamente en un vuelo de Nueva York a Londres, pero se ven obligados a separarse en la aduana.",
+        rating: [
+            { src: "img/star.png", height: undefined },
+            { src: "img/star.png", height: 24 },
+            { src: "img/star.png", height: 24 },
+            {
+                src: "img/star.png", height: 24
+            },
+            { src: "img/star.png", height: 24 }
+        ],
+
+    },
+    {
+        id: 2,
+        titulo: "La sorra de severá",
+        imagenPrincipal: "https://infoliteraria.com/wp-content/uploads/2022/05/la-probabilidad-estaditica.jpg.webp",
+        descripcion: "Hadley y Oliver comienzan a enamorarse lentamente en un vuelo de Nueva York a Londres, pero se ven obligados a separarse en la aduana.",
+        rating: [
+            { src: "img/star.png", height: undefined },
+            { src: "img/star.png", height: 24 },
+            { src: "img/star.png", height: 24 },
+            {
+                src: "img/star.png", height: 24
+            },
+            { src: "img/star.png", height: 24 }
+        ],
+
+    },
+    {
+        id: 3,
+        titulo: "Sintposiano violado de burras",
+        imagenPrincipal: "https://infoliteraria.com/wp-content/uploads/2022/05/la-probabilidad-estaditica.jpg.webp",
+        descripcion: "Hadley y Oliver comienzan a enamorarse lentamente en un vuelo de Nueva York a Londres, pero se ven obligados a separarse en la aduana.",
+        rating: [
+            { src: "img/star.png", height: undefined },
+            { src: "img/star.png", height: 24 },
+
+            {
+                src: "img/star.png", height: 24
+            },
+            { src: "img/star.png", height: 24 }
+        ],
+
+    }
+];
+
+var $lista_catalogo = document.querySelector("#lista_catalogo")
+
+peliculas.forEach(pelicula => {
+    $lista_catalogo.appendChild(crearPeliculas(pelicula))
+})
+
+function crearPeliculas(pelicula) {
+    let $pelicula = document.createElement("li")
+    $pelicula.className = "movie_item"
+
+    let $imagen = document.createElement("img")
+    $imagen.src = pelicula.imagenPrincipal
+    $imagen.alt = pelicula.titulo
+    $pelicula.appendChild($imagen)
+
+    let $titulo = document.createElement("h3")
+    $titulo.textContent = pelicula.titulo
+    $pelicula.appendChild($titulo)
+
+    let $descripcion = document.createElement("p")
+    $descripcion.textContent = pelicula.descripcion
+    $pelicula.appendChild($descripcion)
+
+    let $rating = document.createElement("div")
+    $rating.className = "rating"
+    pelicula.rating.forEach(estrella => {
+        let $estrella = document.createElement("img")
+        $estrella.src = estrella.src
+        $estrella.height = estrella.height
+        $rating.appendChild($estrella)
+    })
+    $pelicula.appendChild($rating)
+
+    return $pelicula
+}
+
+var proximasPeliculas = [
+    {
+        img: "https://archivos-cms.cinecolombia.com/images/1/1/6/3/43611-2-esl-CO/cf1a918e2882-warner_thenun_cinecol_480x670.jpg",
+        label: "Fecha de estreno: 2/10/2023"
+    },
+    {
+        img: "https://pics.filmaffinity.com/Spider_Man_Cruzando_el_Multiverso-257260163-large.jpg",
+        label: "Fecha de estreno: 13/10/2023"
+    }
+
+]
+
+var $upcoming_movies = document.querySelector("#upcoming_movies")
+
+proximasPeliculas.forEach(pelicula => {
+    $upcoming_movies.appendChild(crearProximasPeliculas(pelicula))
+})
+
+function crearProximasPeliculas(pelicula) {
+    let $pelicula = document.createElement("li")
+
+    let $imagen = document.createElement("img")
+    $imagen.className = "next_movie"
+
+    $imagen.src = pelicula.img
+    $imagen.alt = pelicula.label
+    $pelicula.appendChild($imagen)
+
+    let $label = document.createElement("p")
+    $label.textContent = pelicula.label
+    $pelicula.appendChild($label)
+
+    return $pelicula
+}
+
+let $select_pelicula = document.querySelector("#pelicula")
+
+peliculas.forEach(pelicula => {
+    let $option = document.createElement("option")
+    $option.value = pelicula.id
+    $option.textContent = pelicula.titulo
+    $select_pelicula.appendChild($option)
+})
+
+let reservas = localStorage.getItem("reservas") ? JSON.parse(localStorage.getItem("reservas")) : []
+
+let $submit = document.querySelector("#submit")
+
+$submit.onclick = function (event) {
+    event.preventDefault()
+
+    let cedula = document.querySelector("#cedula").value
+    let id_pel = document.querySelector("#pelicula").value
+    let horario = document.querySelector("#hora").value
+
+    let reserva = {
+        cedula: cedula,
+        id_pel: id_pel,
+        nombre: peliculas[id_pel - 1].titulo,
+        horario: horario
+    }
+    console.log(reserva)
+    console.log(cedula);
+    reservas.push(reserva)
+    localStorage.setItem("reservas", JSON.stringify(reservas))
+
+    alert("Reserva exitosa")
+    document.querySelector("#cedula").value = ""
+}
+
+
+
+
+
+
+
